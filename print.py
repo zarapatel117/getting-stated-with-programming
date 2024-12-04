23339bf1fefb535c3727c26e6e8ccc051c990e8e
zarapatel117 <zarapatel2510@gmail.com~>
2024-12-04 19:39:57 +0530

    print statments

---
+++ print.py
@@ -0,0 +1,10 @@
+#print a statment using a string
+print ("hello codingal")
+#print a statment using a integer
+print (335)
+#print a statment using a next line
+print("hello world\n")
+#print a statment using a integer and string
+print("hi mam",321654987)
+#print a statment using a end argument
+print("meow meow ", end="bow")
